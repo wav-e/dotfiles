ed67c78b5cd9ab890118572ac582bf42ca8de2a6
wav-e <wave1992@gmail.com>
2024-01-13 15:32:31 +0400

    WIP input drivers
    WIP trackpad: acceleration like mac debugging

---
+++ input_apple/trackpad/trackpad_x_accel.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+
+# argument {1} - xinput device id
+
+ID=${1}
+
+# xinput list
+# xinput list-props ${ID}
+# xinput set-prop ${ID} "libinput Scrolling Pixel Distance" 50
+#
+#xinput set-prop ${ID} "libinput Accel Custom Motion Points" 0.0, 0.5
+
+
+ACCEL_POINTS="
+0.0
+   0.5203
+   0.9440
+   1.3293
+   1.8497
+   2.5240
+   3.1407
+   3.7380
+   4.4700
+   4.9133
+   5.5300
+   6.3970
+   6.9170
+   7.3410
+   7.6880
+   7.9577
+   8.2273
+   8.4780
+   8.6707
+   8.7860
+   8.8823
+   8.9787
+   9.0750
+   9.1713
+   9.2293
+   9.2677
+   9.2100
+   9.1713
+   9.1330
+   9.0943
+   9.0560
+   8.9597
+   8.8633
+   8.7860
+   8.6897
+   8.5743
+   8.4780
+   8.3430
+   8.2273
+   8.0540
+   7.9383
+   7.8033
+   7.6880
+   7.5723
+   7.4567
+   7.3410
+   7.2447
+   7.1483
+   7.0520
+   6.9557
+"
+
+
+
+xinput set-prop ${ID} "libinput Accel Custom Motion Points" $ACCEL_POINTS 
+xinput set-prop ${ID} "libinput Accel Profile Enabled" 0, 0, 1 
+xinput set-prop ${ID} "libinput Accel Custom Motion Step" 0.8
+
